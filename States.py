668b3ec421f587b6256724f4338398e71f3a3a2c
snarkyBoi <>
2022-04-13 20:00:31 +0000

    Initial commit

---
+++ States.py
@@ -0,0 +1,182 @@
+from pygame import *
+init()
+fontsize = 15
+allSprites = sprite.Group([])
+offset = 0
+class text(sprite.Sprite):
+    def __init__(self,text,x,y,fontsize):
+        self.font = font.SysFont('timesnewroman',fontsize)
+        super().__init__()
+        self.xpos,self.ypos = x,y
+        self.text = []
+        for i in text:
+            self.text.append(self.font.render(i,False,(0,0,0)))
+    def Render(self,window):
+        for i in range(len(self.text)):
+            window.blit(self.text[i],(self.xpos,self.ypos+(fontsize*i)+(7*i)))
+class Object(sprite.Sprite):
+    def __init__(self,name,data):
+        super().__init__()
+        self.image = transform.scale(image.load("Sprites/Select/"+name+".png"),(960,600))
+        self.rect = self.image.get_rect()
+        self.mask = mask.from_surface(self.image)
+        self.state = data
+        allSprites.add(self)
+class State():
+    def __init__(self,name,flag,capitol,capinfo,bird,birdImg,birdsong,flower,flowerImg,fInfo,des):
+        self.data = [name,capitol,capinfo,bird,flower,fInfo,des]
+        self.sprites = sprite.Group([])
+        self.text = sprite.Group([])
+        self.name = None
+        self.flag = None
+        self.capitol = None
+        self.capInfo = None
+        self.bird = None
+        self.birdImg = None
+        self.birdSong = None
+        self.flower = None
+        self.flowerImg = None
+        self.flowerInfo = None
+        self.des = None
+    def Load(self):
+        self.name = text(self.data[0],25,25,50)
+        self.flag = sprite.Sprite()
+        self.flag.image = transform.scale(image.load("Sprites/flags/"+self.data[0]+".png"),(225,150))
+        self.flag.rect = self.flag.image.get_rect(topleft=(25,75))
+        self.capitol = text(self.data[1],300,25,30)
+        self.capInfo = text(self.data[2],300,75,20)
+        self.bird = text(self.data[3],25,375,30)
+        self.birdImg = sprite.Sprite()
+        self.birdImg.image = transform.scale(image.load("Sprites/Birds/"+self.data[0]+".png"),(150,150))
+        self.birdImg.rect = self.birdImg.image.get_rect(topleft=(25,450))
+        self.birdSong = mixer.Sound("Sounds/"+self.data[0]+".mp3")
+        self.flower = text(self.data[4],500,375,30)
+        self.flowerImg = sprite.Sprite()
+        self.flowerImg.image = transform.scale(image.load("Sprites/Flowers/"+self.data[0]+".png"),(150,150))
+        self.flowerImg.rect = self.flowerImg.image.get_rect(topleft=(500,450))
+        self.flowerInfo = text(self.data[5],725,500,20)
+        self.des = text(self.data[6],25,250,15)
+        self.sprites.add(self.flag,self.birdImg,self.flowerImg)
+        self.text.add(self.name,self.capitol,self.capInfo,self.bird,self.flower,self.flowerInfo,self.des)
+        return self
+    def Render(self,window):
+        self.sprites.draw(window)
+        for i in self.text:
+            i.Render(window)
+
+#format:
+# nameData = State([name],flag,[capitol,capitolpop],[capinfoline1,capinfoline2,etc...],[birdname,birdgenus],birdImg,birdSong,[flowername,flowergenus],flowerImg,[flower uses],[desline1,desline2,etc...])
+# name = Object(name,nameData)
+
+#alabama
+alabamaData = State(["Alabama"],"Alabama",["Capitol: Montgomery","Population: 200,603"],["Established in 1846","Rank in state: 17th"],["Yellowhammer (Northern flicker)","Colaptes auratus"],"Alabama","Alabama",["Camellia","Genus: Theaceae"],"alabama-flower",["Medicinal uses:The flowers are astringent, antihemorrhagic, haemostatic, salve and tonic.","When mixed with sesame oil they are used in the treatment of burns and scalds."],["Flag was adopted in 2015","U.S. state flag consisting of a white field with a red saltire (diagonal cross). ","represented the sepration of Alabama from the Union"])
+alabama = Object("Alabama",alabamaData)
+#alaska
+alaskaData = State(["Alaska"],"Alaska",["Capitol: Juneau","Population: 31,275"],["Established in 1900","Rank in state: 45th"],["Willow ptarmigan","Lagopus lagopus"],"Alaska","Alaska",["Alpine Forget-me-not","Genus: Scorpion Grasses"],"Alaska",["Medicinal uses:Used for its therapeutic properties, forget-me-nots can be used as an astringent in poultices for wounds to tighten tissues."],["Flag was adopted in 1964","The colors of the flag symbolize Colorado's geographical features","The gold symbolizes the states abundant sunshine","The white represents snow-capped mountains","The blue symbolizes clear blue skies","The red represents the color of much of the state's soil"])
+alaska = Object("Alaska",alaskaData)
+#arizona
+arizonaData = State(["Arizona"],"Arizona",["Capitol: Phoenix","Population: 1,608,139"],["Established in 1889","Rank in state: 1st"],["Cactus wren","Campylorhynchus brunneicapillus"],"Arizona","Arizona",["Saguaro Cactus Blossom","Genus: Carnegiea family"],"arizona-flower",["Medicinal uses:They have high moisture retention to hydrate your body.","Saguaros are also loaded with Vitamin C, Vitamin B12 and other key minerals","to nurture your organs and enable better functioning."],["Flag was adopted in 1917","The red and yellow also symbolize Arizona's picturesque landscape.","The center star signifies copper production; Arizona produces more copper than any other state in the US.","The white represents snow-capped mountains","The colors of red and blue are the same shade used on the flag of the United States."])
+arizona = Object("Arizona",arizonaData)
+#arkansas
+arkansasData = State(["Arkansas"],"Arkansas",["Capitol: Little Rock","Population: 202,591"],["Established in 1821","Rank in state: 21st"],["Northern mockingbird","Mimus polyglottos"],"Arkansas","Arkansas",["Apple Blossom","Genus: Apple"],"Arkansas",["Medicinal uses: Apple blossoms are generally very high in antioxidants and important in ridding the body of free radicals.","A tea of dried Apple blossoms may be consumed as a stress reliever, digestion aid or to clear complexion."],["Flag was adopted in 1913","The diamond represents Arkansas' status as the only diamond-bearing state in the Union.","25 of white stars around the border of the diamond represent Arkansas' position as the 25th state to join the Union.","The star above ARKANSAS represents the Confederacy.","The three stars on the bottom are for The three nations to which Arkansas has belonged (France, Spain, and the U.S.) The Louisiana Purchase, which brought Arkansas into the U.S was signed in 1803.","Arkansas was the third state (after Louisiana and Missouri) formed from the Louisiana Purchase"])
+arkansas = Object("Arkansas",arkansasData)
+#california
+
+#colorado
+coloradoData = State(["Colorado"],"Colorado",["Capitol: Denver","Population: 715,522"],["Established in 1867","Rank in state: 5th"],["Lark Bunting","Calamospoza Melanocorys"],"Colorado","Colorado",["Rocky Mountain Columbine","flower genus"],"Colorado",["flower uses"],["Flag was adopted in 1964","The colors of the flag symbolize Colorado's geographical features","The gold symbolizes the states abundant sunshine","The white represents snow-capped mountains","The blue symbolizes clear blue skies","The red represents the color of much of the state's soil"])
+colorado = Object("Colorado",coloradoData)
+
+#connecticut
+
+#delaware
+
+#florida
+
+#georgia
+
+#hawaii
+
+#idaho
+idaho = Object("Idaho","test") 
+#illinois
+
+#indiana
+
+#iowa
+
+#kansas
+
+#kentucky
+
+#loisiana
+
+#maine
+
+#maryland
+
+#massachusetts
+
+#michigan
+
+#minnesota
+
+#mississippi
+
+#missouri
+
+#montana
+
+#nebraska
+
+#nevada
+nevada = Object("Nevada","test")
+
+#new hampshire
+
+#new jersey
+
+#new mexico
+
+#new york
+
+#north carolina
+
+#north dakota
+
+#ohio
+
+#oklahoma
+
+#oregon
+
+#pennsylvania
+
+#rhode island
+
+#south carolina
+
+#south dekota
+
+#tennessee
+
+#texas
+
+#utah
+utah = Object("Utah","test")
+
+#vermont
+
+#virginia
+
+#washington
+
+#west virginia
+west_virginiaData = State(["West Virginia"],"West Virginia",["Capitol: Charleston","Population: 51,400"],["Established in 1885","Rank in state: 38th"],["Northern cardinal","Cardinalis cardinalis"
+],"West Virginia","West Virginia",["Rhododendron","Rhododendron"],"West Virginia",["Medicinal uses:Rhododendron is one of the naturally occurring plants which possess various health benefits, such as prevention and treatment of diseases associated with heart, dysentery, diarrhea, detoxification, inflammation, fever, constipation, bronchitis and asthma."],["Flag was adopted in 1964","The colors of the flag symbolize Colorado's geographical features","The gold symbolizes the states abundant sunshine","The white represents snow-capped mountains","The blue symbolizes clear blue skies","The red represents the color of much of the state's soil"])
+colorado = Object("Colorado",coloradoData)
+#wisconsin
+wisconsinData = State(["Wisconsin"],"Wisconsin",["Capitol: Madison","Population: 269,840"],["Established in 1838","Rank in state: 15th"],["American robin","Turdus migratorius"],"Wisconsin","Wisconsin",["Wood Violet","Violet"],"Wisconsin",["Violet is moist and cooling and the leaves ease inflammation, and when used externally, soothe skin irritations and swelling. It has an affinity for the lymphatic system and can promote healthy lymphatic function. "],["The tools of the important trades of the times lie within the shield: the symbols for agriculture (plow), mining (pick and shovel), manufacturing (arm and hammer), and navigation (anchor). The badger, which is the state animal, sits above the shield. It represents the name given to the lead miners ADOPTED IN 1848."])
+wisconsin = Object("Wisconsin",wisconsinData)
+#wyoming
+wyomingData = State(["Wyoming"],"Wyoming",["Capitol: Cheyenne","Population: 64,019"],["Established in 1869","Rank in state: 36th"],["Western meadowlark","Sturnella neglecta"],"Wyoming","Wyoming",["Indian Paintbrush","Castilleja"],"Wyoming",["Medicinal uses: People use the Indian Paintbrush as a medicine to treat rheumatism and as a bath rinse to make their hair glossy."],["Flag was adopted in 1917","The flag of the state of Wyoming consists of the silhouette of an American bison. The red symbolizes the Native Americans and the blood of pioneers who gave their lives. The white is a symbol of purity and uprightness."])
+wyoming = Object("Wyoming",wyomingData)
---
+++ main.py
@@ -0,0 +1,69 @@
+from States import *
+from pygame import *
+from time import sleep
+init()
+win = display.set_mode((960,600))
+display.set_caption("States")
+states = []
+select = True
+state = None
+def GetTrans(sprite):
+    new = sprite
+    new.image.convert_alpha()
+    new.image.set_alpha(100)
+    return new
+def GetNorm(sprite):
+    new = sprite
+    new.image.convert_alpha()
+    new.image.set_alpha(255)
+    return new
+class Cursor(sprite.Sprite):
+    def __init__(self,cursor):
+        super().__init__()
+        self.image = transform.scale(image.load("Sprites/"+cursor+".png"),(2,2)).convert_alpha().set_alpha(0)
+        self.rect = self.image.get_rect(center=mouse.get_pos())
+        self.mask = mask.from_surface(self.image)
+    def Render(self):
+        self.rect = self.image.get_rect(center=mouse.get_pos())
+        win.blit(self.image,self.rect)
+    def Collide(self):
+        global allSprites
+        for i in allSprites:
+            if sprite.collide_mask(self,i) != None:
+                allSprites.remove(i)
+                i = GetTrans(i)
+                allSprites.add(i)
+                if mouse.get_pressed()[0] == True:
+                    global select, state
+                    state = i.state
+                    select = False
+            else:
+                allSprites.remove(i)
+                i = GetNorm(i)
+                allSprites.add(i)
+running = True
+cursor = Cursor("cursor")
+offset = 0
+while running:
+    win.fill((255,255,255))
+    for i in event.get():
+        if i.type == QUIT:
+            running = False
+    if key.get_pressed()[K_q]:
+        running = False
+    if select == True:
+        allSprites.draw(win)
+        cursor.Collide()
+    else:
+        state.Load().Render(win)
+        if key.get_pressed()[K_b]:
+            select = True
+            state.birdSong.set_volume(0)
+        if key.get_pressed()[K_p]:
+            state.birdSong.set_volume(100)
+            state.birdSong.play()
+        if key.get_pressed()[K_s]:
+            state.birdSong.set_volume(0)
+    cursor.Render()
+    display.flip()
+    sleep(0.01)
